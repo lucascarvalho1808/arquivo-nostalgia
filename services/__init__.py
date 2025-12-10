e58a87059769c5bfe36af73122b94fdc799aaf4c
Lucas Carvalho <lucas.carvalho180807@gmail.com>
2025-12-10 09:14:13 -0300

    feat: adiciona importações das funções de API TMDB e RAWG

---
+++ services/__init__.py
@@ -0,0 +1,12 @@
+from .api_tmdb import (
+    buscar_filmes_populares, 
+    buscar_series_populares, 
+    pesquisar_midia, 
+    buscar_detalhes_filme
+)
+
+from .api_rawg import (
+    buscar_jogos_populares, 
+    pesquisar_jogos, 
+    buscar_detalhes_jogo
+)
